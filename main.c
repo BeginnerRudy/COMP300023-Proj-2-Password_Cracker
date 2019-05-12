19ce8e46a789acfedbd3fce9902daeedf58cf95d
renjiem <renjiem@student.unimelb.edu.au>
2019-05-12 16:34:57 +1000

    Finish crack with no arg

--- main.c
+++ main.c
@@ -9,102 +9,89 @@
 /****************************** MACROS ******************************/
 #define PWD4SHA256_PATH "./pwd4sha256"
 #define PWD6SHA256_PATH "./pwd6sha256"
+#define GUESS_FAILED "F"
 
-#define NUM_OF_PWD4 10
-#define NUM_OF_PWD6 20
 #define SIZE_OF_SHA256 32
 
 
 
-#define ASCII_START 32 + 65
+// #define ASCII_START 32 + 65
+#define ASCII_START 32
 #define ASCII_END 126
 
+typedef struct{
+    BYTE** pwd_sha256_list;
+    int pwd_count;
+}pwd_sha256_t;
+
 /*********************** FUNCTION PROTOTYPES ***********************/
-BYTE** pwd_reader(char* filePath, int num_of_pwd);
+pwd_sha256_t* pwd_reader(char* filePath);
 void pwd_printer(char* pwd);
 BYTE* str_to_sha_256(char* str);
-char* is_cracked(BYTE** pwdNsha256, int num_of_pwd, char* guess);
+char* is_cracked(pwd_sha256_t* pwdNsha256, char* guess);
 void print_hash(BYTE* hash);
 
 /*********************** FUNCTION DEFINITIONS ***********************/
 int main(int argc, char* argv[]){
     /* read pwd4sha256*/
-    // BYTE** pwd4sha256 = pwd_reader(PWD4SHA256_PATH, NUM_OF_PWD4);
-    BYTE** pwd6sha256 = pwd_reader(PWD6SHA256_PATH, NUM_OF_PWD6);
 
-    if (argc == 3){
-        char* guesses_filepath = argv[1];
-        char* pwdNsha256_filepath = argv[2];
-        printf("%d arguments\n", argc);
-    }else if (argc == 1){
-        /* use sha256 to generate hash*/
-
-        // FILE * fp;
-        // char * line = NULL;
-        // size_t len = 0;
-        // ssize_t read;
-        //
-        // fp = fopen(argv[1], "r");
-        // if (fp == NULL){
-            //     exit(EXIT_FAILURE);
-            // }
-            // while ((read = getline(&line, &len, fp)) != -1) {
-                //     printf("%s", line);
-                // }
-                //
-                // fclose(fp);
-                // if (line){
-                    //     free(line);
-                    // }
-                    // exit(EXIT_SUCCESS);
-
-
-                    // char* word = (char*)malloc(4*sizeof(char));
-                    // for (int i = ASCII_START; i <= ASCII_END; i++){
-                        //     word[0] = i;
-                        //     for (int j = ASCII_START; j <= ASCII_END; j++){
-                            //         word[1] = j;
-                            //         for (int k = ASCII_START; k <= ASCII_END; k++){
-                                //             word[2] = k;
-                                //             for (int p = ASCII_START; p <= ASCII_END; p++){
-                                    //                 word[3] = p;
-                                    //                 char* res = is_cracked(pwd4sha256, NUM_OF_PWD4, word);
-                                    //             }
-                                    //         }
-                                    //     }
-                                    // }
-                                    char* word = (char*)malloc(6*sizeof(char));
-                                    for (int i = ASCII_START; i <= ASCII_END; i++){
-                                        word[0] = i;
-                                        for (int j = ASCII_START; j <= ASCII_END; j++){
-                                            word[1] = j;
-                                            for (int k = ASCII_START; k <= ASCII_END; k++){
-                                                word[2] = k;
-                                                for (int p = ASCII_START; p <= ASCII_END; p++){
-                                                    word[3] = p;
-                                                    for (int q = ASCII_START; q <= ASCII_END; q++){
-                                                        word[4] = q;
-                                                        for (int z = ASCII_START; z<= ASCII_END; z++){
-                                                            word[5] = z;
-                                                            is_cracked(pwd6sha256, NUM_OF_PWD6, word);
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-
-                                    // char* res = is_cracked(pwd6sha256, NUM_OF_PWD6, "renjie");
-
-                                    /* generate all combinations of 4*/
-
-                                    /* make them into sha256*/
-
-                                    /* start cracking*/
-                                    // how to check equlity
+    if (argc == 1){
+        pwd_sha256_t* pwd4sha256 = pwd_reader(PWD4SHA256_PATH);
+        pwd_sha256_t* pwd6sha256 = pwd_reader(PWD6SHA256_PATH);
+        // hack 4 char pwd first
+        char* word4 = (char*)malloc(4*sizeof(char));
+        word4[4] = '\0'; // terminate at 5th char
+        for (int i = ASCII_START; i <= ASCII_END; i++){
+            word4[0] = i;
+            for (int j = ASCII_START; j <= ASCII_END; j++){
+                word4[1] = j;
+                for (int k = ASCII_START; k <= ASCII_END; k++){
+                    word4[2] = k;
+                    for (int p = ASCII_START; p <= ASCII_END; p++){
+                        word4[3] = p;
+                        is_cracked(pwd4sha256, word4);
+                        // printf("%s\n", word4);
+                    }
+                }
+            }
+        }
+        // then, hack 6 char pwd
+        // char* word6 = (char*)malloc(6*sizeof(char));
+        // word6[6] = '\0'; // terminate at 7th char
+        // for (int i = ASCII_START; i <= ASCII_END; i++){
+        //     word6[0] = i;
+        //     for (int j = ASCII_START; j <= ASCII_END; j++){
+        //         word6[1] = j;
+        //         for (int k = ASCII_START; k <= ASCII_END; k++){
+        //             word6[2] = k;
+        //             for (int p = ASCII_START; p <= ASCII_END; p++){
+        //                 word6[3] = p;
+        //                 for (int q = ASCII_START; q <= ASCII_END; q++){
+        //                     word6[4] = q;
+        //                     for (int z = ASCII_START; z<= ASCII_END; z++){
+        //                         word6[5] = z;
+        //                         is_cracked(pwd6sha256, word6);
+        //                     }
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
 
     }else if(argc == 2){
         int total_num_guesses = atoi(argv[1]);
+        printf("%d arguments\n", argc);
+    }else if (argc == 3){
+        char* guesses_filepath = argv[1];
+        char* pwdNsha256_filepath = argv[2];
+
+        // read pwdNsha256 file into 2D array of BYTES
+        pwd_sha256_t* pwdNsha256 = pwd_reader(pwdNsha256_filepath);
+        // read guess file
+        // take out one guess
+        // hash it to SHA256
+        // compare with pwdNsha256
+        // result
         printf("%d arguments\n", argc);
     }
 
@@ -112,8 +99,8 @@ int main(int argc, char* argv[]){
 }
 
 /*This function is responsible for read the pwdNsha256 into a 2D array of BYTE*/
-BYTE** pwd_reader(char* filePath, int num_of_pwd){
-
+pwd_sha256_t* pwd_reader(char* filePath){
+    // read the file and get the file length
     unsigned char * buffer = 0;
     long length;
     FILE * f = fopen (filePath, "rb");
@@ -129,27 +116,25 @@ BYTE** pwd_reader(char* filePath, int num_of_pwd){
     fclose (f);
     }
 
-    int count;
-    if (num_of_pwd == NUM_OF_PWD6){
-        count = 20;
-    }else if(num_of_pwd == NUM_OF_PWD4){
-        count = 10;
-    }
+    // delcare and initialize for storing sha256 of pwd
+    int count = length/SIZE_OF_SHA256;
+    pwd_sha256_t* pwdNsha256 = (pwd_sha256_t*)malloc(sizeof(pwd_sha256_t));
+    pwdNsha256->pwd_sha256_list = (BYTE**)malloc(count*sizeof(BYTE*));
+    pwdNsha256->pwd_count = count;
 
-    BYTE** pwdNsha256 = (BYTE**)malloc(count*sizeof(BYTE*));
     for (int i = 0; i < count; i++){
-        pwdNsha256[i] = (BYTE*)malloc(SIZE_OF_SHA256*sizeof(BYTE));
+        pwdNsha256->pwd_sha256_list[i] = (BYTE*)malloc(SIZE_OF_SHA256*sizeof(BYTE));
     }
 
-    // print out the result
+    // storing
     if (buffer){
-        for (int i = 0; i < num_of_pwd; i++){
+        for (int i = 0; i < count; i++){
             for (int j = 0; j < 32; j++){
-                pwdNsha256[i][j] = buffer[i*SIZE_OF_SHA256+j];
+                pwdNsha256->pwd_sha256_list[i][j] = buffer[i*SIZE_OF_SHA256+j];
             }
-            // print_hash(pwdNsha256[i/8]);
         }
     }
+
     return pwdNsha256;
 }
 
@@ -179,14 +164,22 @@ void print_hash(BYTE* hash){
     printf("-\n");
 }
 
-char* is_cracked(BYTE** pwdNsha256, int num_of_pwd, char* guess){
+/*This function takes guess and sha256 of pwds and check if the guess matches*/
+char* is_cracked(pwd_sha256_t* pwdNsha256, char* guess){
+    // hash the guess to get its sha256
     BYTE* hash_guess = str_to_sha_256(guess);
-    for (int i = 0; i < num_of_pwd; i++){
-        if (memcmp(pwdNsha256[i], hash_guess, SIZE_OF_SHA256) == 0){
-            printf("%s %d\n", guess, i+11);
+    // compare the sha256 of guess to each pwd's sha256
+    for (int i = 0; i < pwdNsha256->pwd_count; i++){
+        // if guess matches, print it out and return it
+        if (memcmp(pwdNsha256->pwd_sha256_list[i], hash_guess, SIZE_OF_SHA256) == 0){
+            printf("%s %d\n", guess, i);
+            free(hash_guess);
             return guess;
         }
     }
+    // free guess
     free(hash_guess);
-    return "N";
+
+    // guess matches nothing, return GUESS_FAILED
+    return GUESS_FAILED;
 }
